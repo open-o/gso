ab8a6502aa77acd07c2051bd8cc03ca49fd41681
c00149107 <chenchuanyu@huawei.com>
2016-10-26 19:08:08 +0800

    fix issue:GSO-59
    Change-Id: I18c1fa995fd3b434d9fa1707122169ad5cc8dec0
    Signed-off-by: c00149107 <chenchuanyu@huawei.com>

--- gso_commsvc_svcmgr_service/service/src/test/resources/inventory.sql
+++ gso_commsvc_svcmgr_service/service/src/test/resources/inventory.sql
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-drop table gso_lcm_servicebaseinfo if exists;
-create table gso_lcm_servicebaseinfo
+drop table t_lcm_servicebaseinfo if exists;
+create table t_lcm_servicebaseinfo
 (
     serviceId    varchar(255)  not null,
 	serviceName  varchar(255)  not null,
@@ -24,24 +24,24 @@ create table gso_lcm_servicebaseinfo
 	status       varchar(20)  not null,	
 	creator      varchar(50)  not null,
 	createdTime  bigint       not null,
-	constraint gso_lcm_servicebaseinfo primary key(serviceId)
+	constraint t_lcm_servicebaseinfo primary key(serviceId)
 );
-drop table gso_lcm_defPackage_mapping if exists;
-create table gso_lcm_defPackage_mapping
+drop table t_lcm_defPackage_mapping if exists;
+create table t_lcm_defPackage_mapping
 (
     serviceId     varchar(255)  not null,
 	serviceDefId  varchar(255)  not null,
 	templateId    varchar(255)   not null,
 	templateName  varchar(255)  not null,
 	constraint gso_p_defPackage_mapping primary key(serviceId),
-	constraint gso_f_defPackage_mapping foreign key(serviceId) references gso_lcm_servicebaseinfo (serviceId)
+	constraint gso_f_defPackage_mapping foreign key(serviceId) references t_lcm_servicebaseinfo (serviceId)
 );
-drop table gso_lcm_inputParam_mapping if exists;
-create table gso_lcm_inputParam_mapping
+drop table t_lcm_inputParam_mapping if exists;
+create table t_lcm_inputParam_mapping
 (
     serviceId   varchar(255)  not null,
 	inputKey    varchar(255)  not null,
 	inputValue  varchar(255)   not null,	
 	constraint gso_p_inputParam_mapping primary key(serviceId,inputKey),
-	constraint gso_f_inputParam_mapping foreign key(serviceId) references gso_lcm_servicebaseinfo (serviceId)
+	constraint gso_f_inputParam_mapping foreign key(serviceId) references t_lcm_servicebaseinfo (serviceId)
 );
