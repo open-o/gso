b5cc162e3cd65c58f550a83dccec95f8222e4954
luniHw <luna.lu@huawei.com>
2017-01-11 12:00:36 +0800

    Modify database script
    Add operation tables and modify service segment table construct
    
    Issue ID:GSO-159
    Signed-off-by: luniHw <luna.lu@huawei.com>
    Change-Id: Iaf1ea9da3be53d2c75c9d4ccf6d61cf236fbdf5a

--- gso_commsvc_svcmgr_service/deployment/src/main/release/init/servicemanagerservice_tables_mysql.sql
+++ gso_commsvc_svcmgr_service/deployment/src/main/release/init/servicemanagerservice_tables_mysql.sql
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Huawei Technologies Co., Ltd.
+ * Copyright 2016-2017 Huawei Technologies Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,15 +20,14 @@ drop table if exists t_lcm_service;
 
 create table t_lcm_service
 (
-    serviceId    varchar(255) not null,
+    serviceId    varchar(36) not null,
     name         varchar(255) not null,
-	description  varchar(255) null,
-	activeStatus varchar(255) not null,
-	status       varchar(255) not null,
-	creator      varchar(255) not null,
-	createAt     bigint(13)   not null,
-	result       varchar(255) not null,
-	primary key(serviceId)
+    description  varchar(255) null,
+    activeStatus varchar(20) not null,
+    status       varchar(20) not null,
+    creator      varchar(50) not null,
+    createAt     bigint(13)   not null,
+    primary key(serviceId)
 )
 engine=innodb;
 
@@ -37,11 +36,10 @@ drop table if exists t_lcm_defPackage_mapping;
  
 create table t_lcm_defPackage_mapping
 (
-    serviceId    varchar(255) not null,
-	serviceDefId varchar(255) not null,
-	templateId  varchar(255)  not null,
-	templateName varchar(255) not null,
-	primary key(serviceId)
+    serviceId    varchar(36) not null,
+    serviceDefId varchar(255) not null,
+    templateId  varchar(255)  not null,
+    primary key(serviceId)
 )
 engine=innodb;
 
@@ -50,10 +48,10 @@ drop table if exists t_lcm_service_parameter;
 
 create table t_lcm_service_parameter
 (
-    serviceId    varchar(255) not null,
-	paramName varchar(255) not null,
-	paramValue  varchar(255) not null,	
-	constraint pk_t_parameters primary key(serviceId, paramName)
+    serviceId    varchar(36) not null,
+    paramName varchar(36) not null,
+    paramValue  mediumtext not null,    
+    constraint pk_t_parameters primary key(serviceId, paramName)
 )
 engine=innodb;
 
@@ -62,14 +60,51 @@ drop table if exists t_lcm_service_segment;
 
 create table t_lcm_service_segment
 (
-    serviceId    varchar(255) not null,
-	serviceSegmentId varchar(255) not null,
-	serviceSegmentName  varchar(255) ,
-	templateId varchar(255) not null,
-	nodeType varchar(255) not null,	
-	topoSeqNumber int not null,
-	status varchar(20) ,
-	constraint pk_t_segments primary key(serviceId, serviceSegmentId)
+    serviceId    varchar(36) not null,
+    serviceSegmentId varchar(255) not null,
+    serviceSegmentType varchar(20) not null,
+    serviceSegmentName  varchar(255) ,
+    templateId varchar(255) not null,
+    nodeType varchar(255) not null,    
+    topoSeqNumber int not null,
+    domainHost varchar(100),
+    nodeTemplateName varchar(255) not nulll,
+    constraint pk_t_segments primary key(serviceId, serviceSegmentId, serviceSegmentType)
+)
+engine=innodb;
+
+
+drop table if exists t_lcm_svcsegment_operation;
+
+create table t_lcm_svcsegment_operation
+(
+    serviceSegmentId varchar(255) not null,
+    serviceSegmentType varchar(20)  not null,
+    jobId  varchar(36) ,
+    status varchar(20) ,
+    progress int,    
+    errorCode int,
+    statusDescription varchar(255) ,
+    constraint pk_t_seg_operation primary key(serviceSegmentId, serviceSegmentType)
+)
+engine=innodb;
+
+
+drop table if exists t_lcm_service_operation;
+
+create table t_lcm_service_operation
+(
+    serviceId varchar(36) not null,
+    operationId varchar(36) not null,
+    operationType varchar(36)  not null,
+    userId  varchar(36) ,
+    result varchar(20) not null,
+    operationContent varchar(255) ,
+    progress int not null,
+    reason varchar(255),
+    operationAt bigint(13) not null,
+    finishedAt bigint(13) not null,
+    constraint pk_t_svc_operation primary key(serviceId, opertaionId)
 )
 engine=innodb;
 
